72bbff3e18f54a6d16e2868db7794dd2a0aed5d1
Mabdelshafy <mrmohamedabdelshafy@gmail.com>
2023-10-07 10:59:03 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main function
- * 
- * Description: entry point 
+* main - Entry point
+* Reutrn: 0 (Success)
 */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
